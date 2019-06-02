c609b50f53e534b6c7b923dc0f9bcc7333c1c5a6
AlejandroMu <alejandrobravo0329@gmail.com>
2019-06-02 17:24:35 -0500

    server

--- clienterest/src/main/java/edu/icesi/clienterest/Delegado/PacienteDelegado.java
+++ clienterest/src/main/java/edu/icesi/clienterest/Delegado/PacienteDelegado.java
@@ -3,6 +3,7 @@ package edu.icesi.clienterest.Delegado;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import edu.icesi.clienterest.model.*;
 
@@ -12,11 +13,15 @@ import edu.icesi.clienterest.model.*;
 @Service
 public class PacienteDelegado {
 
+	private RestTemplate template;
+
 	public List<Pacient> getPacients() {
 		return null;
 	}
 
 	public Pacient getPacient(String string) {
+
+		Pacient resultado = template.getForObject("https://serverrestpacientes.herokuapp.com/pacientes?", Pacient.class);
 		return null;
 	}
 
